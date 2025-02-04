aa8d51b76914c944730adb53db24242e7fcf943b
adkinsar <adkinsar3@gmail.com>
2025-02-04 17:27:21 -0500

    Add role for GitHub Actions to assume

--- lib/stacks/ActionsStack.ts
+++ lib/stacks/ActionsStack.ts
@@ -1,11 +1,13 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class ActionsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const oidcProvider = new cdk.aws_iam.OpenIdConnectProvider(
+    const oidcProvider = new iam.OpenIdConnectProvider(
       this,
       "GithubOidcProvider",
       {
@@ -13,5 +15,75 @@ export class ActionsStack extends cdk.Stack {
         clientIds: ["sts.amazonaws.com"],
       }
     );
+
+    const accessAnalyzerArtifactBucket = new s3.Bucket(
+      this,
+      "AccessAnalyzerArtifactBucket",
+      {
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+        encryption: s3.BucketEncryption.S3_MANAGED,
+        versioned: false, // this would be true in enterprise
+        removalPolicy: cdk.RemovalPolicy.DESTROY, // this would be RETAIN in enterprise
+        autoDeleteObjects: true, // this would be false in enterprise
+        minimumTLSVersion: 1.2,
+        enforceSSL: true,
+      }
+    );
+
+    const accessAnalyzerPermissionsPolicy = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          actions: [
+            "iam:GetPolicy",
+            "iam:GetPolicyVersion",
+            "access-analyzer:ListAnalyzers",
+            "access-analyzer:ValidatePolicy",
+            "access-analyzer:CreateAccessPreview",
+            "access-analyzer:GetAccessPreview",
+            "access-analyzer:ListAccessPreviewFindings",
+            "access-analyzer:CreateAnalyzer",
+            "access-analyzer:CheckAccessNotGranted",
+            "access-analyzer:CheckNoNewAccess",
+            "s3:ListAllMyBuckets",
+            "cloudformation:ListExports",
+            "ssm:GetParameter",
+            "cloudformation:CreateStack",
+            "cloudformation:DescribeStacks",
+            "cloudformation:CreateChangeSet",
+            "cloudformation:DescribeChangeSet",
+            "cloudformation:DeleteChangeSet",
+            "cloudformation:ExecuteChangeSet",
+            "iam:CreateRole",
+            "iam:PutRolePolicy",
+          ],
+          effect: iam.Effect.ALLOW,
+          resources: ["*"],
+        }),
+        new iam.PolicyStatement({
+          actions: ["s3:ListObjects", "s3:GetObject", "s3:PutObject"],
+          effect: iam.Effect.ALLOW,
+          resources: [
+            accessAnalyzerArtifactBucket.arnForObjects("*"),
+            accessAnalyzerArtifactBucket.bucketArn,
+          ],
+        }),
+      ],
+      assignSids: true,
+    });
+
+    const roleForGitHubActions = new iam.Role(this, "RoleForGitHubActions", {
+      assumedBy: new iam.WebIdentityPrincipal(
+        oidcProvider.openIdConnectProviderArn,
+        {
+          StringLike: {
+            "token.actions.githubusercontent.com:sub": "repo:soapy-codes/*", // TODO
+          },
+        }
+      ),
+      inlinePolicies: {
+        AccessAnalyzerPermissionsPolicy: accessAnalyzerPermissionsPolicy,
+      },
+      roleName: "GitHubActionsIAMAccessAnalzer",
+    });
   }
 }
