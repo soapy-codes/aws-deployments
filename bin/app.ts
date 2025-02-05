b76435f433821cb3f20d7f8f4626e2e9b95e1ce9
adkinsar <adkinsar3@gmail.com>
2025-02-05 09:09:27 -0500

    Add jq for pretty event printing, CDK_DEFAULT and stackName variables in stack invocation

--- bin/app.ts
+++ bin/app.ts
@@ -3,4 +3,10 @@ import * as cdk from "aws-cdk-lib";
 import { ActionsStack } from "../lib/stacks/ActionsStack";
 
 const app = new cdk.App();
-new ActionsStack(app, "GitHubActionsStack", {});
+new ActionsStack(app, "GitHubActionsStack", {
+  stackName: "GitHubActionsStack",
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
